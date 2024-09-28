b4896b5b303f7d18aeba756ec80009a5f5b5cd4e
srinu <srinu.devops9553@gmail.com>
2024-09-28 12:43:46 -0400

    terraform code main.tf update

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_vpc" "srinu" {
     Name = "srinu"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public"
+  }
+}
